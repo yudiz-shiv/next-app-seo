ad69ae5c722cebb0038faae3b94abbaa05dee623
yudiz-shiv <shivkumar.prajapat@yudiz.com>
2023-11-23 16:16:17 +0530

    fix: 🐛 og image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello Next js 14 SEO</h1>;
+  return (
+    <h1 className="text-5xl font-bold text-center mt-5">
+      Hello Next js 14 SEO <code className="bg-red-200">Metadata</code>
+    </h1>
+  );
 }
