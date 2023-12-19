22bba1436ea131ad7ec76ec8db33694f3f574f81
yudiz-shiv <shivkumar.prajapat@yudiz.com>
2023-12-19 17:51:05 +0530

    feat: 🎸 react-typed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import Typed from "react-typed";
+
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold text-center mt-5">
-      Hello Next js 14 SEO <code className="bg-red-200">Metadata</code>
-    </h1>
+    <div>
+      <h1 className="text-5xl font-bold text-center mt-5">
+        Hello Next js 14 SEO <code className="bg-red-200">Metadata</code>
+      </h1>
+      <Typed
+        strings={[
+          "Front End Developer",
+          "UI Developer",
+          "React JS Developer",
+          "Next JS Developer",
+        ]}
+        typeSpeed={100}
+        className="text-3xl xl:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-yellow-500 md:text-start"
+      />
+    </div>
   );
 }
