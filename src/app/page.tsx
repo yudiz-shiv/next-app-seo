be657962b78e355cf39949bb6498ff1f15262449
yudiz-shiv <shivkumar.prajapat@yudiz.com>
2023-12-19 17:58:09 +0530

    feat: 🎸 typed.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,44 @@
 "use client";
 
-import Typed from "react-typed";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 export default function Home() {
+  // Create Ref element.
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Smart Backspace First",
+        "Smart Backspace Second",
+        "Candy",
+        "More Strings",
+      ], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      smartBackspace: true,
+      loop: true,
+      showCursor: true,
+      cursorChar: "!",
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <div>
       <h1 className="text-5xl font-bold text-center mt-5">
         Hello Next js 14 SEO <code className="bg-red-200">Metadata</code>
       </h1>
-      <Typed
-        strings={[
-          "Front End Developer",
-          "UI Developer",
-          "React JS Developer",
-          "Next JS Developer",
-        ]}
-        typeSpeed={100}
-        className="text-3xl xl:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-yellow-500 md:text-start"
-      />
+      <div>
+      <h1 ref={el}></h1>
+      </div>
     </div>
   );
 }
