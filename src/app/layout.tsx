76773194874d356703374a846dd84b2b436084d9
Shivkumar Prajapat <104143801+yudiz-shiv@users.noreply.github.com>
2023-12-26 17:45:47 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,17 +27,17 @@ export const metadata: Metadata = {
     description:
       "Explore Sophie Smith's frontend developer portfolio, featuring a mastery of responsive web design, sleek UI/UX, and cutting-edge technologies. Elevate your digital projects with a developer committed to creating visually stunning and seamlessly functional websites. Contact Sophie for collaborative opportunities in bringing your web presence to the forefront of innovation.",
     url: "https://shivprajapat.vercel.app",
-    // images: "https://raw.githubusercontent.com/yudiz-shiv/next-app-seo/main/public/twitter.jpg",
+    // images: "./twitter.jpg",
     locale: "en_US",
     type: "website",
     images: [
       {
-        url: "https://lh3.googleusercontent.com/drive-viewer/AEYmBYSW0J89JkC3o8j0MJ6Ih6s40cfvcnjxgguL8toDC0zYZF0BldZzfh0hM_gpKxUvlrssoCk7W-fBanBarVVhlsFJEKv6KQ=w1920-h857",
+        url: "./twitter.jpg",
         width: 800,
         height: 600,
       },
       {
-        url: "https://lh3.googleusercontent.com/drive-viewer/AEYmBYSW0J89JkC3o8j0MJ6Ih6s40cfvcnjxgguL8toDC0zYZF0BldZzfh0hM_gpKxUvlrssoCk7W-fBanBarVVhlsFJEKv6KQ=w1920-h857",
+        url: "./twitter.jpg",
         width: 1800,
         height: 1600,
         alt: "My custom alt",
